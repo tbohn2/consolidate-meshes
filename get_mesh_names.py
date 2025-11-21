bca570fbdbdfba9dc085fbabefb9726ab0e68689
tbohn2 <tanner.bohn@yahoo.com>
2025-11-21 15:16:23 -0700

    Script to get all mesh names in blend file

---
+++ get_mesh_names.py
@@ -0,0 +1,18 @@
+# Script to get all mesh names; copy, paste, and run in blender
+
+print("=== Mesh Objects in this .blend file ===")
+
+import bpy
+
+meshes = "["
+
+mesh_objs = [obj.name for obj in bpy.data.objects if obj.type == 'MESH']
+
+for i, name in enumerate(mesh_objs):
+    meshes += f"\"{name}\""
+    if i < len(mesh_objs) - 1:
+        meshes += ", "
+
+meshes += "]"
+
+print(meshes)
