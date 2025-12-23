4fcface9a194f7c1d2d60225d92d5ac8a17c80ae
tbohn2 <tanner.bohn@yahoo.com>
2025-12-22 20:08:17 -0700

    Changed format for printing mesh names

--- get_mesh_names.py
+++ get_mesh_names.py
@@ -4,15 +4,18 @@ print("=== Mesh Objects in this .blend file ===")
 
 import bpy
 
-meshes = "["
+# meshes = "["
+print("[")
 
 mesh_objs = [obj.name for obj in bpy.data.objects if obj.type == 'MESH']
 
 for i, name in enumerate(mesh_objs):
-    meshes += f"\"{name}\""
-    if i < len(mesh_objs) - 1:
-        meshes += ", "
+    # meshes += f"\"{name}\""
+    # if i < len(mesh_objs) - 1:
+    #     meshes += ", "
+    print(f"\"{name}\",") 
 
-meshes += "]"
+print("]")
+# meshes += "]"
 
-print(meshes)
+# print(meshes)
